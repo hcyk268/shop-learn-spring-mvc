3066c7f6dfafb39b520fdb279ca58be7841087fb
hcyk268 <vvhipt206.contact@gmail.com>
2025-11-24 21:24:19 +0700

    Dash board admin

---
+++ src/main/java/com/haipro/shop/controller/admin/DashBoardController.java
@@ -0,0 +1,14 @@
+package com.haipro.shop.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class DashBoardController {
+    @GetMapping("/admin")
+    public String getDashBoard() {
+        return "admin/dashboard/overview";
+    }
+
+}
